6220755017acb5f224ce0a1e644efd2af855014e
Alex Addy <alex.addy@gmail.com>
2017-02-20 21:15:50 -0500

    Moved glider_test into tests folder

---
+++ tests/glider.rs
@@ -0,0 +1,30 @@
+extern crate gameoflife;
+
+use gameoflife::simulate;
+
+// [0, 1] == row 0 column 1
+
+#[test]
+fn glider_test() {
+    let mut gen_0 = [[0u8; 10]; 10];
+    // __X
+    // X_X
+    // _XX
+    gen_0[0][2] = 1;
+    gen_0[1][0] = 1;
+    gen_0[1][2] = 1;
+    gen_0[2][1] = 1;
+    gen_0[2][2] = 1;
+    let mut gen_1 = [[0u8; 10]; 10];
+    // _X__
+    // __XX
+    // _XX_
+    gen_1[0][1] = 1;
+    gen_1[1][2] = 1;
+    gen_1[1][3] = 1;
+    gen_1[2][1] = 1;
+    gen_1[2][2] = 1;
+
+    let res_1 = simulate::get_next_gen(&gen_0);
+    assert_eq!(gen_1, res_1);
+}
