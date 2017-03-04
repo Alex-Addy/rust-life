a070921cdc874191be88a2327c4a5504fdbe6ada
Alex Addy <alex.addy@gmail.com>
2017-03-03 22:12:36 -0500

    simulation now handles variably sized boards

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ use gameoflife::simulate::Simulation;
 use gameoflife::display;
 
 fn main() {
-    let mut gen_0 = [[0u8; 10]; 10];
+    let mut gen_0 = vec![vec![0u8; 40]; 30];
     // __X
     // X_X
     // _XX
@@ -24,13 +24,13 @@ fn main() {
     let mut field = simulate::Conway::new(gen_0);
     let mut stdout = io::stdout();
 
-    for _ in 0..12 {
+    for _ in 0..40 {
         print!("{}{}", cursor::Goto(1, 1), clear::AfterCursor);
         field.advance(1);
         display::dump_board(&mut stdout, field.state()).unwrap_or_else(|e: io::Error| {
             println!("{}", e);
         });
-        thread::sleep(time::Duration::from_millis(1000));
+        thread::sleep(time::Duration::from_millis(500));
     }
 
 }
--- src/simulate/mod.rs
+++ src/simulate/mod.rs
@@ -4,7 +4,7 @@ pub trait Simulation {
     fn advance(&mut self, generations: u8);
 }
 
-type Board = [[u8; 10]; 10];
+type Board = Vec<Vec<u8>>;
 
 #[derive(Debug)]
 enum FrontField {
@@ -24,8 +24,8 @@ impl Conway {
     pub fn new(start: Board) -> Conway {
         Conway {
             current_field: FrontField::FieldOne,
+            board_2: vec![vec![0u8; start[0].len()]; start.len()],
             board_1: start,
-            board_2: [[0u8; 10]; 10],
         }
     }
 
@@ -35,72 +35,7 @@ impl Conway {
             FrontField::FieldTwo => (&self.board_2, &mut self.board_1),
         };
 
-        for row in 0..10 {
-            for col in 0..10 {
-                let mut alive = 0;
-                // test up
-                if row > 0 {
-                    // test up
-                    if from[row - 1][col] == 1 {
-                        alive += 1;
-                    }
-                    // test up left
-                    if col > 0 {
-                        if from[row - 1][col - 1] == 1 {
-                            alive += 1;
-                        }
-                    }
-                    // test up right
-                    if col < 10 - 1 {
-                        if from[row - 1][col + 1] == 1 {
-                            alive += 1;
-                        }
-                    }
-                }
-                // test down
-                if row < 10 - 1 {
-                    // test down
-                    if from[row + 1][col] == 1 {
-                        alive += 1;
-                    }
-                    // test down left
-                    if col > 0 {
-                        if from[row + 1][col - 1] == 1 {
-                            alive += 1;
-                        }
-                    }
-                    // test down right
-                    if col < 10 - 1 {
-                        if from[row + 1][col + 1] == 1 {
-                            alive += 1;
-                        }
-                    }
-                }
-
-                // test left
-                if col > 0 {
-                    if from[row][col - 1] == 1 {
-                        alive += 1;
-                    }
-                }
-                // test right
-                if col < 10 - 1 {
-                    if from[row][col + 1] == 1 {
-                        alive += 1;
-                    }
-                }
-
-                if alive == 0 {
-                    to[row][col] = 0;
-                } else if alive == 2 {
-                    to[row][col] = from[row][col];
-                } else if alive == 3 {
-                    to[row][col] = 1;
-                } else {
-                    to[row][col] = 0;
-                }
-            }
-        }
+        calc_gen(from, to);
 
         match self.current_field {
             FrontField::FieldOne => {
@@ -113,22 +48,90 @@ impl Conway {
     }
 }
 
-impl Simulation for Conway {
-    fn state(&self) -> Vec<Vec<u8>> {
-        let b = match self.current_field {
-            FrontField::FieldOne => self.board_1,
-            FrontField::FieldTwo => self.board_2,
-        };
-        let mut out = Vec::with_capacity(10);
-        for row in b.iter() {
-            let mut r = Vec::with_capacity(10);
-            for cell in row.iter() {
-                r.push(*cell);
+// calc_gen calculates the new generation using
+// prev and places the new gen into next
+// assumes prev and next have the same sizes
+// assumes row and column number greater than 0
+// assumes rectangle with all columns having the same height
+fn calc_gen(prev: &Board, next: &mut Board) {
+    let row_num = prev.len();
+    let col_num = prev[0].len();
+    for row in 0..row_num {
+        for col in 0..col_num {
+            let mut alive = 0;
+            // test up
+            if row > 0 {
+                // test up
+                if prev[row - 1][col] == 1 {
+                    alive += 1;
+                }
+                // test up left
+                if col > 0 {
+                    if prev[row - 1][col - 1] == 1 {
+                        alive += 1;
+                    }
+                }
+                // test up right
+                if col < col_num - 1 {
+                    if prev[row - 1][col + 1] == 1 {
+                        alive += 1;
+                    }
+                }
             }
-            out.push(r);
-        }
+            // test down
+            if row < row_num - 1 {
+                // test down
+                if prev[row + 1][col] == 1 {
+                    alive += 1;
+                }
+                // test down left
+                if col > 0 {
+                    if prev[row + 1][col - 1] == 1 {
+                        alive += 1;
+                    }
+                }
+                // test down right
+                if col < col_num - 1 {
+                    if prev[row + 1][col + 1] == 1 {
+                        alive += 1;
+                    }
+                }
+            }
 
-        out
+            // test left
+            if col > 0 {
+                if prev[row][col - 1] == 1 {
+                    alive += 1;
+                }
+            }
+            // test right
+            if col < col_num - 1 {
+                if prev[row][col + 1] == 1 {
+                    alive += 1;
+                }
+            }
+
+            if alive == 0 {
+                next[row][col] = 0;
+            } else if alive == 2 {
+                next[row][col] = prev[row][col];
+            } else if alive == 3 {
+                next[row][col] = 1;
+            } else {
+                next[row][col] = 0;
+            }
+        }
+    }
+}
+
+impl Simulation for Conway {
+    fn state(&self) -> Board {
+        let b = match self.current_field {
+            FrontField::FieldOne => &self.board_1,
+            FrontField::FieldTwo => &self.board_2,
+        };
+
+        b.clone()
     }
 
     fn advance(&mut self, generations: u8) {
